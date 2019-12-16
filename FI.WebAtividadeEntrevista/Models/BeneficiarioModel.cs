04c67c61035e9549cb720a8a9fa2d8530953ec43
Cleiton Ferreira <csferreiraa@gmail.com>
2019-12-16 00:49:06 -0300

    Estruturando Beneficiario

---
+++ FI.WebAtividadeEntrevista/Models/BeneficiarioModel.cs
@@ -0,0 +1,29 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace WebAtividadeEntrevista.Models
+{
+    /// <summary>
+    /// Classe de Modelo do Beneficiario
+    /// </summary>
+    public class BeneficiarioModel
+    {
+        public long Id { get; set; }
+
+        /// <summary>
+        /// IdCliente
+        /// </summary>
+        public long IdCliente { get; set; }
+
+        /// <summary>
+        /// Nome
+        /// </summary>
+        [Required]
+        public string Nome { get; set; }
+
+        /// <summary>
+        /// CPF
+        /// </summary>
+        [Required]
+        public string CPF { get; set; }
+    }
+}
